4857aaa8950185e8351e59b526e5cdfbb775fb2f
revynxi <revynxi@gmail.com>
2025-02-01 21:11:39 +0500

    Preventing bot from falling asleep

--- brain.py
+++ brain.py
@@ -1,10 +1,23 @@
+from flask import Flask
 import os
 import discord
 from discord import app_commands
 from discord.ext import commands
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is now awake"
+
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+    
+
 intents = discord.Intents.default()
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix=">>", intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -19,5 +32,9 @@ async def on_ready():
 @bot.tree.command(name="awake", description="Hey, Adroit, are you awake?")
 async def hello(interaction: discord.Interaction):
     await interaction.response.send_message(f"Awake. Never Sleep.")
+    
+
+Thread(target=run_flask).start()
+
 
 bot.run(os.getenv("ADROIT_TOKEN"))
